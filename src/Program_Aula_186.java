115b803b8e7e91ffc6df6dd6c90a1b15537073f3
Mauricio PC Casa <mauriciozaccaro@gmail.com>
2021-04-21 15:26:10 -0400

    Aula 186 - Criando, recriando e alterando arquivo texto com BufferedWriter e FileWriter

---
+++ src/Program_Aula_186.java
@@ -0,0 +1,27 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program_Aula_186 {
+
+	public static void main(String[] args) {
+		
+		String[] lines = new String[] {"Good Morning", "Good Afternoon", "Good nigth"};	
+		
+		String path = "C:\\Drivers PC\\out.txt";
+		
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){ // esse "true" no final faz com que o arquivo não seja recriado, mas sim alterado
+			for(String line : lines) { // para cada string line existente no vetor lines
+				bw.write(line);
+				bw.newLine();
+			}
+			bw.write("");
+			bw.newLine();
+		}
+		catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+
+	}
+
+}

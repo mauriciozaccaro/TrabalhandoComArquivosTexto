be1ed632333fe18700466b6a05d0ab7ccdc86b16
Mauricio PC Casa <mauriciozaccaro@gmail.com>
2021-04-21 14:52:31 -0400

    Limpando o codigo com 'try with resources' na Aula 185

--- src/Program_Aula_184.java
+++ src/Program_Aula_184.java
@@ -7,17 +7,10 @@ public class Program_Aula_184 {
 	public static void main(String[] args) {
 		
 		String path = "C:\\Drivers PC\\teste_java.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
 		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr); // BufferedReader é instaciado a partir do FileReader
-			// poderia ser feito em uma linha única, conforme abaixo
-			//  br = new BufferedReader(new FileReader(path));
+		try(BufferedReader br = new BufferedReader(new FileReader(path))) {
 			
 			String line = br.readLine();
-			
 			while (line != null) {
 				System.out.println(line);
 				line = br.readLine();
@@ -26,21 +19,5 @@ public class Program_Aula_184 {
 		catch(IOException e) {
 			System.out.println("Error: " + e.getMessage());
 		}
-		finally{
-			try {
-				if(br != null ) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			}catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-
-
 	}
-
 }

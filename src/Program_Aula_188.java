3f010271c93de071cc0e29244cc300cb4b12be32
Mauricio PC Casa <mauriciozaccaro@gmail.com>
2021-04-21 22:39:27 -0400

    Alteração na Aula 188 - adicionado campo para ler a data de criação e ultima modificação do arquivo

--- src/Program_Aula_188.java
+++ src/Program_Aula_188.java
@@ -1,5 +1,10 @@
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Program_Aula_188 {
@@ -13,6 +18,26 @@ public class Program_Aula_188 {
 			String strPath = sc.nextLine();
 			// sc = C:\Drivers PC\out.txt
 			File path = new File(strPath);
+			
+			BasicFileAttributes att;
+			att = Files.readAttributes(path.toPath(), BasicFileAttributes.class);
+			BasicFileAttributes atttt;
+			atttt = Files.readAttributes(path.toPath(), BasicFileAttributes.class);
+			
+			FileTime tFile = att.creationTime();
+			FileTime ttFile = atttt.lastModifiedTime();
+			
+
+		    String pattern = "dd/MM/yyyy HH:mm:ss";
+		    SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+			
+		    String formatted = simpleDateFormat.format( new Date( tFile.toMillis() ) );
+		    String formatted2 = simpleDateFormat.format( new Date( ttFile.toMillis() ) );
+		    
+		    
+		    System.out.println("Data e hora de criação do arquivo: " + formatted);
+		    System.out.println("Data e hora da ultima alteração do arquivo: " + formatted2);
+		    System.out.println();
 			
 			System.out.println("getName: " + path.getName()); // getName traz apenas o nome do arquivo
 			System.out.println("getParent: " + path.getParent()); // getParent traz apenas o caminho do arquivo
